ddc1dde706846670a91f939522241f403250c1eb
Rad Hoda <57790085+rodhoda@users.noreply.github.com>
2024-02-18 19:24:09 -0800

    Added WIP Code
    Updated the sigmoid transfer function to also allow the return of its derivative using a bool parameter
    
    Created a backward() function where back propagation will take place. I have written out the formulas and my conceived understanding of the code in the comments.
    
    NOTE, I'm still debugging with it so the hidden layer weight update is not working, please comment out before using. I also have not implemented the bias but once the weight works the bias should be easy. 
    
    I thought I would share what I've done with you while I'm still debugging so you at least get some perspective and sanity-check it.

---
+++ BackpropLayer_Update.m
@@ -0,0 +1,168 @@
+classdef BackpropLayer < handle
+    properties
+        inputSize
+        hiddenSize
+        outputSize
+        %layers, each has weights, bias, and transfer function
+        hiddenLayer
+        outputLayer
+        learningRate
+        
+        % Activation Output for each layer
+        hiddenOutput
+        finalOutput
+
+        % Current input given 
+        inputPattern;
+
+    end
+
+    methods
+        %% constructor
+        function this = BackpropLayer(inputCount, hiddenCount, outputCount, learnRate)
+            % setup size counts
+            this.inputSize = inputCount;
+            this.outputSize = outputCount;
+            this.hiddenSize = hiddenCount;
+
+            % randomize weights
+            this.hiddenLayer.weights = randn(this.hiddenSize, this.inputSize);
+            this.outputLayer.weights = randn(this.outputSize, this.hiddenSize);
+
+            % randomize bias
+            this.hiddenLayer.bias = randn(this.hiddenSize, 1);
+            this.outputLayer.bias = randn(this.outputSize, 1);
+
+            % set inital learning rate
+            this.learningRate = learnRate;
+        end
+
+        %% forward, create the output of the layer passed an input set
+        function this = forward(this, input)
+            this.inputPattern = input;
+            % Compute hidden layer input
+            hiddenInput = this.hiddenLayer.weights * input + this.hiddenLayer.bias;
+            % Hidden layer activation value
+            this.hiddenOutput = this.doFunc((hiddenInput), this.hiddenLayer.transferFunc);
+
+            % Compute output layer output
+            finalInput = this.outputLayer.weights * this.hiddenOutput + this.outputLayer.bias;
+            % Output layer activation value
+            this.finalOutput = this.doFunc((finalInput), this.outputLayer.transferFunc);
+            output = this.finalOutput;
+        end
+
+        %factory function to help forward, send to correct function
+        function func = doFunc(this, n, do)
+            switch do
+                case "hardlim"
+                    func = this.hardlim(n);
+                case "hardlims"
+                    func = this.hardlims(n);
+                case "purelin"
+                    func = this.purelin(n);
+                case "logsig" %log sigmoid
+                    func = this.sigmoid(n);
+                otherwise
+                    error("Transfer function not supported.");
+            end
+        end
+
+        %hard limit
+        function f = hardlim(this, n)
+            f = n >= 0;
+        end
+
+        %symetrical hard limit
+        function f = hardlims(this, n)
+            if(n < 0)
+                f = -1;
+            else % n >=0
+                f = 1;
+            end
+        end
+
+        %linear
+        function f = purelin(this, n)
+            f = n;
+        end
+
+        %log sigmoid
+        % We will also be taking the derivative as this
+        % is our function for the hidden layer 
+        % We have added a boolean parameter to indicate
+        % when to return the derived f 
+        function f = sigmoid(this, n, deriv)
+            if nargin <3
+                deriv = false; % Defualt if not provided
+            end
+            denom = 1 + exp(-n);
+            sigmoidVal = 1 ./ denom;
+            if deriv
+                f = sigmoidVal / (1 - sigmoidVal);
+            else
+                f = sigmoidVal;
+            end
+        end
+
+        %% use backprop learning to update the weights
+        function this = train(this, input, target)
+
+        end
+
+        %% set functions
+        function this = setLearningRate(this, newRate)
+            this.learningRate = newRate;
+        end
+
+        function this = setHiddenLayerSize(this, newSize)
+            this.hiddenSize = newSize;
+        end
+
+        % Backward function that will utilize backpropagation
+        function this = backward(this, testPattern)
+            % Find the error in the output layer
+            % outputError = t - a
+            outputError = testPattern - this.finalOutput';
+
+            % This will be the derivative of our f(n) function
+            derivOutput = this.sigmoid(this.finalOutput,true);
+
+            % This computes the sensitivity of the output layer
+            % S(m+1) =  -2 * f'(n) * e(t-a)
+            outputSensitivity = derivOutput * outputError';
+
+            disp("BEFORE UPDATE")
+            disp(this.outputLayer.weights);
+            
+            %% Update Outer Layer Weights
+            % We can now use outputSensitivity to update the weight
+            % of our output layer: W(m+1) = W(m+1) - learningRate * S(m+1) * P
+            val = (outputSensitivity * this.hiddenOutput');
+            finalValue = this.learningRate .* val;
+            this.outputLayer.weights = this.outputLayer.weights - finalValue;
+
+            %% Update Hidden Layer Weights
+            % First we need to get the sensitivity of the
+            % hidden layer and its precursors
+            % S(m) = S(m+1) * W(m+1) * f'(m)
+            derivHidden = this.sigmoid(this.hiddenOutput, true);
+            hiddenSensitivity = outputSensitivity * this.hiddenLayer.weights * derivHidden;
+
+            this.hiddenLayer.weights = this.hiddenLayer.weights - this.learningRate * (hiddenSensitivity * this.inputPattern');
+
+
+            
+        end
+
+    end
+
+    methods(Access=private)
+        %mean-squared error function
+        function error = msError(observed, predicted)
+            residual = observed - predicted;
+            error = power(residual, 2);
+        end
+    end
+
+end
---
+++ hotCoding_Update.m
@@ -0,0 +1,25 @@
+%% setup layer
+% Initialize the network with an input size of 30
+% a hidden layer of 10 neurons, and an output layer
+% equal to the number of values being identified
+network = BackpropLayer(30, 10, 3, 0.1);
+network.outputLayer.transferFunc = "logsig";
+network.hiddenLayer.transferFunc = "logsig";
+
+%% input patterns
+p0 = [-1 1 1 1 -1 1 -1 -1 -1 1 1 -1 -1 -1 1 1 -1 -1 -1 1 1 -1 -1 -1 1 -1 1 1 1 -1];
+p1 = [-1 1 1 -1 -1 -1 -1 1 -1 -1 -1 -1 1 -1 -1 -1 -1 1 -1 -1 -1 -1 1 -1 -1 -1 -1 1 -1 -1];
+p2 = [1 1 1 -1 -1 -1 -1 -1 1 -1 -1 -1 -1 1 -1 -1 1 1 -1 -1 -1 1 -1 -1 -1 -1 1 1 1 1];
+p = [p0' p1' p2'];
+
+% target patterns
+t0 = [1 0 0]';
+t1 = [0 1 0]';
+t2 = [0 0 1]';
+t = [t0 t1 t2];
+
+%% train layer
+network.forward(p0');
+
+
+network.backward(t0');
